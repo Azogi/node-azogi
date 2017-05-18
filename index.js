266fe52fee59fe8c55a5418e2fd627fd73c4dc4c
Yeechan Lu <wz.bluesnow@gmail.com>
2017-05-18 16:11:01 +0800

    Release v0.0.2

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-function randomItem(array) {
+const Bluebird = require('bluebird')
+
+const randomItem = function (array) {
   return array[Math.floor((Math.random() * array.length))]
-} 
+}
+
+let defineSyncAndAsyncFunction = function (klass, name, generatorFunction) {
+  Object.defineProperty(generatorFunction, "name", { value: name + "Generator" })
+
+  let syncFunction = function() {
+    let generator = generatorFunction(this, true, ...arguments)
+    let value = {}
+    while (true) {
+      value = generator.next(value.value)
+      if (value.done) {
+        return value.value
+      }
+    }
+  }
+  Object.defineProperty(syncFunction, "name", { value: name })
+  klass.prototype[name] = syncFunction
+
+  let asyncPromise = Bluebird.coroutine(generatorFunction)
+  let asyncFunction = function() {
+    return asyncPromise(this, false, ...arguments)
+  }
+  Object.defineProperty(asyncFunction, "name", { value: name + "Async" })
+  klass.prototype[name + "Async"] = asyncFunction
+}
 
 class Azogi {
   constructor() {
@@ -31,31 +57,55 @@ class Azogi {
 
   next() {
     let result = this.library.shift()
-    if (!result)
-      throw new Error('Running out of materials')
-    return result
+    if (result) return result
+
+    this.onExhausted()
+    result = this.library.shift()
+    if (result) return result
+ 
+    throw new Error('Running out of materials')
   }
 
-  nextParagraph(maxLength = 80, minLength = 40) {
+  async nextAsync() {
+    let result = this.library.shift()
+    if (result) return result
+
+    await this.onExhaustedAsync()
+    result = this.library.shift()
+    if (result) return result
+ 
+    this.onExhausted()
+    result = this.library.shift()
+    if (result) return result
+
+    throw new Error('Running out of materials')
+  }
+
+  onExhausted() {}
+  async onExhaustedAsync() {}
+}
+
+defineSyncAndAsyncFunction(Azogi, "nextParagraph", 
+  function *(self, sync, maxLength = 80, minLength = 40) {
     if (maxLength < minLength)
       throw new RangeError('maxLength must be greater than minLength')
 
-    let res = this.next()
+    let res = yield (sync ? self.next() : self.nextAsync())
     while (true) {
-      let ne = this.next()
+      let ne = yield (sync ? self.next() : self.nextAsync())
       let nes = randomItem(["，", "，", "，", "，", "，", "。", "。", "。", "。", "？", "！", "……", "：", "；"]) + ne
       if (Math.random() > 0.5 && res.length > minLength) break
 
       if (res.length + nes.length < maxLength - 5) {
         res += nes
       } else {
-        this.pushOne(ne)
+        self.pushOne(ne)
       }
     }
     res += randomItem(["。", "。", "。", "。", "？", "！", "……"])
     return res
   }
-}
+)
 
 module.exports = Azogi
 Azogi.Azogi = Azogi
